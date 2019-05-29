c3a6d734f8b60d8133903aec0da0657319384bba
liuxyjlu <xueyanliu90@gmail.com>
2019-05-29 23:50:57 +0800

    Delete CompP.m

--- CompP.m
+++
@@ -1,9 +0,0 @@
-%%
-%%%%%% calculation of the probablity of A_ij %%%%%%
-%%%%%% input: Theta, Z                       %%%%%%
-%%%%%% output: the prediction of A: P        %%%%%%
-function P = CompP(Theta,Z)
-    P = Z*Theta; % the generitive probability of links
-end
-
-
